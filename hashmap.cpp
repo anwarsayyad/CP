644ce77af62513c099f4894cc6cd6f71cd75d82a
anwarsayyad <anwarsayyad2631@gmail.com>
2024-07-24 21:54:15 +0530

    added hash with number

--- hashmap.cpp
+++ hashmap.cpp
@@ -3,19 +3,30 @@ using namespace std;
 
 void hashmap(){
 	int n;
-	cin>> n;
-
-	int a[n];
+	cin >> n;
+	int arr[n];
 	
-	for(int i=0; i<n; i++){
-		cin >>a[i];
+	for(int i=0; i <n;i++){
+		cin >> arr[i];
 	}
 
-    for(int n: a){
-        cout << n << endl;
-    }
+	//precalculate
+	int hash[20] = {0};
+	for(int i=0; i<n;i++){
+		hash[arr[i]] += 1;
+	}
+	
 
+	int q;
+	cin >> q;
+	while(q--){
+		int n;
+		cin >>n;
 
+		//print
+		cout<<hash[arr[n]]<<endl;
+	}
+	
 }
 
 int main(){
